3ff0fb3459edc4593bfb8391a0de61dfd8e68231
Samuel Juárez Gordon <9005596@alumnos.ufv.es>
2022-12-27 18:47:21 +0100

    Modificadas las interfaces de inicio y el fomulario de añadir pacientes. Creadas las interfaces de pacientes t

--- inicio.php
+++ inicio.php
@@ -7,11 +7,9 @@
     <title>Remind Me</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
     <link rel='stylesheet' type='text/css' media='screen' href='css/start.css'>
     <script src='js/main.js'></script>
@@ -22,13 +20,10 @@
         <div class="container-fluid">
             <div class="container-fluid divLogo">
                 <a class="navbar-brand" href="#">
-                    <img src="media/rmlogo.png" alt="Logo" class="logo" width="40" height="40" class="d-inline-block">
-                    Remind Me
+                    <img src="media/rmlogo.png" alt="Logo" class="logo" width="40" height="40" class="d-inline-block"> Remind Me
                 </a>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -37,7 +32,7 @@
                         <a class="nav-link active" aria-current="page" href="#">Pacientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Perfil</a>
+                        <a class="nav-link" href="">Perfil</a>
                     </li>
 
                 </ul>
@@ -47,14 +42,7 @@
     </nav>
 
     <div class="row">
-        <div class="col-12" align=center>
-            <!-- botón de añadir paciente -->
-            <a href='anadir.php' >
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Añadir Paciente
-            </button>
-            </a>
-        </div>
+
         <?php
             $conn = mysqli_connect("79.146.193.17","admin","admin","RemindMe");
 
@@ -73,14 +61,22 @@
             echo '<h4>'.$row['nombre'].'</h4>';
             echo '<p>Habitacion '.$row['habitacion'].'<br>'.$row['edad'].' Años<br>'.$row['medicamentos'].'</p>';
             echo '<p>';
-            echo '<a href="#" class="btn btn-primary" role="button">Perfil</a>';
-            echo '<a href="#" class="btn btn-primary" role="button" id="Eliminar" data-id="'.$row['idPacientes'].'">Eliminar</a>';            
+            echo '<a href="paciente1.php" class="btn btn-primary" role="button">Perfil</a>';
+            echo '<a href="#" class="btn btn-danger" role="button" id="Eliminar" data-id="'.$row['idPacientes'].'">Eliminar</a>';            
             echo '</div>';
             echo '</div>';
             echo '</div>';
             }// Cerramos la conexión a la base de datos
             mysqli_close($conn);
         ?>
+    </div>
+    <div class="col-12" align=center>
+        <!-- botón de añadir paciente -->
+        <a href='anadir.php'>
+            <button type="button" class="btn btn-primary aniadir" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <p>+</p>
+        </button>
+        </a>
     </div>
 </body>
 
